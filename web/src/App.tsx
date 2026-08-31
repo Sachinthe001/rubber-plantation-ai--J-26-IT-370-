04812eaedf10b821ebe9701fc68c27c7bf2b8143
Sachinthe Gamachchi <sachinthawg36@gmail.com>
2026-08-31 19:15:48 +0530

    Fix project setup: install React, configure JSX, wire up App component

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10">
+      <div className="bg-emerald-600 text-white text-xl font-bold p-6 rounded-lg">
+        If this box is green with rounded corners, React + Tailwind both work!
+      </div>
+    </div>
+  )
+}
+
+export default App
